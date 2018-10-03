13a3632c2684f938f1bee9d799d3cddc54e49dca
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2018-10-03 20:57:41 +0300

    Update 1nn.R

--- 1nn.R
+++ 1nn.R
@@ -10,7 +10,7 @@ xl<-(iris[,3:5])
 OY<-c(seq(from=0.0, to=3.0, by=0.1))
 OX<-c(seq(from=0.0, to=7.0, by=0.1))
 
-sortObjectsByDist <- function(xl, point, metricFunction = euclideanDistance)имиьтт
+sortObjectsByDist <- function(xl, point, metricFunction = euclideanDistance)
 
 {
      l <- dim(xl)[1]    
