7a345176dff47d3f1083c7c45b67361da4267c87
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2018-10-22 19:31:12 +0300

    Update knn.R

--- knn.R
+++ knn.R
@@ -1,6 +1,7 @@
 ##Рисуем выборку
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
-plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1, 
+     main = "Задача классификации kNN", xlab = "Длина листа", ylab = "Ширина листа")
 
 ## Сортируем объекты согласно расстояния до объекта z
 sortObjectsByDist <- function(xl, z, metricFunction = euclideanDistance) ## задаем функцию расстояния
